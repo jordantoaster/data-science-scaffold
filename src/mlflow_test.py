b332f3056bc4272c94e882e4d09a01c5256239b4
Jordan McDonald <jordanmcdonaldmain@gmail.com>
2023-10-14 19:50:50 +0100

    feat: lots of ml runs

--- src/mlflow_test.py
+++ src/mlflow_test.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Assumes server running locally.
 mlflow.set_tracking_uri("http://127.0.0.1:5000/")
 
-exp_name = "exp1" 
+exp_name = "exp2" 
 mlflow.set_experiment(exp_name)
 
 run_name = 'run1'
